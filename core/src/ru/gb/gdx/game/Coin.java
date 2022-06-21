23dca0f5553976b1ecaafe9d1bf7c624d78ee46c
W0iD <xcrypter@gmail.com>
2022-06-21 09:39:48 +0300

    homework4

---
+++ core/src/ru/gb/gdx/game/Coin.java
@@ -0,0 +1,51 @@
+package ru.gb.gdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Coin {
+    private AnimPlayer animPlayer;
+    private Vector2 position;
+    private Rectangle rectangle;
+
+
+    public Rectangle getRectangle() {return rectangle;}
+
+    public Coin(Vector2 position) {
+        animPlayer = new AnimPlayer("Full Coinss.png", 8, 1, 10, Animation.PlayMode.LOOP);
+        this.position = new Vector2(position);
+        rectangle = new Rectangle(position.x, position.y, animPlayer.getFrame().getRegionWidth(), animPlayer.getFrame().getRegionWidth());
+    }
+
+    public void draw(SpriteBatch batch, OrthographicCamera camera){
+        animPlayer.step(Gdx.graphics.getDeltaTime());
+        float cx = (position.x - camera.position.x)/camera.zoom + Gdx.graphics.getWidth()/2;
+        float cy = (position.y - camera.position.y)/camera.zoom + Gdx.graphics.getHeight()/2;
+
+        batch.draw(animPlayer.getFrame(), cx, cy);
+
+    }
+
+    public void shapeDraw(ShapeRenderer renderer, OrthographicCamera camera) {
+        float cx = (rectangle.x - camera.position.x)/camera.zoom + Gdx.graphics.getWidth()/2;
+        float cy = (rectangle.y - camera.position.y)/camera.zoom + Gdx.graphics.getHeight()/2;
+        renderer.rect(cx, cy, rectangle.getWidth(), rectangle.getHeight());
+    }
+
+    public boolean isOverlaps(Rectangle heroRect, OrthographicCamera camera){
+        float cx = (rectangle.x - camera.position.x)/camera.zoom + Gdx.graphics.getWidth()/2;
+        float cy = (rectangle.y - camera.position.y)/camera.zoom + Gdx.graphics.getHeight()/2;
+        Rectangle rect = new Rectangle(cx, cy, rectangle.width, rectangle.height);
+        return rect.overlaps(heroRect);
+    }
+
+    public void dispose(){
+        animPlayer.dispose();
+    }
+}
+
---
+++ core/src/ru/gb/gdx/game/Pers.java
@@ -0,0 +1,57 @@
+package ru.gb.gdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Pers {
+    private AnimPlayer idle, jump, walkRight;
+    private boolean isJump, isWalk, dir;
+    private Vector2 pos;
+    private Rectangle rect;
+
+    public Pers() {
+        idle = new AnimPlayer("hero/idle.png", 1, 1, 16.0f, Animation.PlayMode.LOOP);
+        jump = new AnimPlayer("hero/jump.png", 1, 1, 16.0f, Animation.PlayMode.LOOP);
+        walkRight = new AnimPlayer("hero/runRight.png", 4, 1, 16.0f, Animation.PlayMode.LOOP);
+        pos = new Vector2(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        rect = new Rectangle(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2,walkRight.getFrame().getRegionWidth(),walkRight.getFrame().getRegionHeight());
+    }
+
+    public void setWalk(boolean walk) {isWalk = walk;}
+
+    public void setDir(boolean dir) {this.dir = dir;}
+
+    public TextureRegion getFrame(){
+        TextureRegion tmpTex = null;
+        if (!isJump && !isWalk && !dir) {
+            idle.step(Gdx.graphics.getDeltaTime());
+            if (idle.getFrame().isFlipX())
+            idle.getFrame().flip(true, false);
+            tmpTex = idle.getFrame();
+        } else if (!isJump && !isWalk && dir) {
+            idle.step(Gdx.graphics.getDeltaTime());
+            if (!idle.getFrame().isFlipX())
+            idle.getFrame().flip(true, false);
+            tmpTex = idle.getFrame();
+        } else  if (!isJump && isWalk && !dir) {
+            walkRight.step(Gdx.graphics.getDeltaTime());
+            if (walkRight.getFrame().isFlipX())
+            walkRight.getFrame().flip(true, false);
+            tmpTex = walkRight.getFrame();
+        } else  if (!isJump && isWalk && dir) {
+            walkRight.step(Gdx.graphics.getDeltaTime());
+            if (!walkRight.getFrame().isFlipX())
+            walkRight.getFrame().flip(true, false);
+            tmpTex = walkRight.getFrame();
+        }
+        return tmpTex;
+    }
+
+    public Vector2 getPos() {return pos;}
+
+    public Rectangle getRect() {return rect;}
+}
+
